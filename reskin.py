21188b8e095b4d2401d6459f6c39cd815ac9c5ba
JamesV <jvanstone.work@gmail.com>
2019-10-22 08:02:33 +0100

    Attempt to fix the reskin.py, still not working due to some random issue with colours and the way in which they are stored or something. I have no idea what the problem is or how to fix it, but I made some progress. The program will now isolate specific colours and can replace them upon prompting. However, I can't figure out how to control the colour they are replaced with.

--- reskin.py
+++ reskin.py
@@ -9,16 +9,36 @@ import sys, pygame, colours
 from pygame.locals import *
 
 
-def change_colour(image: pygame.Surface, colour):
-    image_array = pygame.PixelArray(image)
-    print(image_array.shape)
-    image_array.replace(colours.blue, colours.green, 0, 0)
-    pygame.image.save(image_array, "sprite.jpg")
+def find_colours(image: pygame.Surface):
+    list_of_colours = []
+    for x in range (image.get_size()[0]):
+        for y in range(image.get_size()[1]):
+            pos = (x, y)
+            pixel = image.get_at(pos)
+            if pixel not in list_of_colours:
+                list_of_colours.append(pixel)
+            image.set_at(pos, pixel)
+    return list_of_colours
+
+
+def change_colours(image: pygame.Surface, colour):
+    for x in range (image.get_size()[0]):
+        for y in range(image.get_size()[1]):
+            pos = (x, y)
+            pixel_col = image.get_at(pos)
+            if pixel_col == colour:
+                pixel_col.b = 30
+            image.set_at(pos, pixel_col)
+    pygame.image.save(image, "reskin.png")
 
 
 def tool(image: str):
     image = pygame.image.load(image)
-    change_colour(image, colours.blue)
+    colour_list = find_colours(image)
+    print(colour_list)
+    colour_to_replace = colour_list[int(input("Select one of the colours to replace: "))-1]
+    change_colours(image, colour_to_replace)
+
 
 
 tool("assets/sans.png")
