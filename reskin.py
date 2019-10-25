67b5af7ca648121e6efe708c2b3aab57abd82bbb
JamesV <JV228882@falmouth.ac.uk>
2019-10-25 12:21:55 +0100

    Removed thorpy.

--- reskin.py
+++ reskin.py
@@ -10,7 +10,6 @@ Each unit of these teams will have to be saved in a new png file."""
 import pygame
 import sys
 import colours
-import thorpy
 
 
 def find_colours(image: pygame.Surface) -> list:
@@ -40,14 +39,13 @@ def change_colours(image: pygame.Surface, colour_to_change: pygame.Color, target
     pygame.image.save(image, "assets/reskin.png")
     return image
 
-
 def tool(image: str):
     """This procedure is a tool for the re-skinning of simple images.
     It will not work well for images with many different colours.
     Procedure includes a pygame window, displaying the images."""
 
     pygame.init()
-    window = pygame.display.set_mode((600,300))
+    window = pygame.display.set_mode((600, 300))
 
     image = pygame.image.load(image).convert()  # Load the image from file.
 
@@ -61,12 +59,12 @@ def tool(image: str):
     image = change_colours(image, colour_to_replace, colours.yellow)  # currently just using yellow for development.
 
     window.blit(pygame.transform.scale(image, (300, 300)), (300, 0))  # Draw reskinned image to screen.
+    pygame.display.flip()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        pygame.display.flip()
 
 
 tool("assets/sans.png")  # Pre-made simple sprite, included in the /assets folder. Simply for testing purposes.
